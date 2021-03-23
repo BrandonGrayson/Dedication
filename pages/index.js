52c3b4e0a26fbeb6b0c481216ca77164e2b50fb2
Brandon <brandonbgrayson@gmail.com>
2021-03-22 20:12:26 -0400

    First Successful Launch of GraphQL Playground

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
 import { Flex } from '@chakra-ui/react';
 import { Layout } from '@components/Layout';
+// apollo server
+
+
+
 
 export default function Index() {
+  const { ApolloServer, gql } = require('apollo-server');
+  const typeDefs = gql `
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+  `;
+
+  const books = [
+    {
+     title: 'The Awakening',
+     author: 'Kate Chopin'
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster'
+    }
+  ]
+
+  const resolvers = {
+    Query: {
+      books: () => books
+    }
+  }
+
+  const server = new ApolloServer ({typeDefs, resolvers});
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  })
+
   return (
     <Layout>
       <Flex direction="column">Space X</Flex>
