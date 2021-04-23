6fc1002684fce39c8987f9c00d5eef594db21642
Brandon <brandonbgrayson@gmail.com>
2021-04-22 23:02:23 -0400

    able to display victory lap album cover

--- src/components/VictoryLap.js
+++ src/components/VictoryLap.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Image, Box } from "@chakra-ui/react";
+import Image from 'next/image'
 
 
 export default function VictoryLap() {
     return (
         <div>
-            <Box boxSize="sm">
-                <Image src="../victory-lap.jpg" alt="Segun Adebayo" />
-            </Box>
+            <Image
+                src="/img/victory-lap.jpg"
+                alt="Picture of the author"
+                width={500}
+                height={500}
+            />
         </div>
     )
 }
