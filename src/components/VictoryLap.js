e2ee4d085e8fa91cbcc1a7593ff3f21d35f73b62
Brandon <brandonbgrayson@gmail.com>
2021-05-04 03:55:16 -0400

    destructuring props from VictoryLap component

--- src/components/VictoryLap.js
+++ src/components/VictoryLap.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from 'next/image';
 
 
-export default function VictoryLap() {
+export default function VictoryLap({props}) {
     return (
         <div>
             <Image
