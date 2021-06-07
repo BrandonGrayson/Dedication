c48b2ea8d24e3138aadb794f58f023cf03f36a33
Brandon <brandonbgrayson@gmail.com>
2021-06-06 22:01:54 -0400

    Header now has a link for Artists page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { list, ListItem } from "@chakra-ui/react"
 
 import Link from "next/link"
 
 export default function Header() {
     return (
-        
+        <ListItem>
+            <Link href='Artist' >
+                <a>Artist</a>
+            </Link>
+        </ListItem>
+
     )
 }
